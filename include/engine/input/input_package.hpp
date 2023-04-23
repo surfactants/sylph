a9eb8be8ea87255e73fe1a4e009e759b073ace46
Surfactants <averillbar@gmail.com>
2023-04-23 05:49:49 -0400

    removing vestigial functions

--- include/engine/input/input_package.hpp
+++ include/engine/input/input_package.hpp
@@ -3,7 +3,6 @@
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Mouse.hpp>
 
-#include <any>
 #include <functional>
 #include <map>
 #include <string>
@@ -34,9 +33,6 @@ public:
 
     void release(sf::Keyboard::Key k);
     void release(sf::Mouse::Button b);
-
-    void setNumPress(sf::Keyboard::Key, std::function<void(unsigned int)> func);
-    void setNumRelease(sf::Keyboard::Key, std::function<void(unsigned int)> func);
 
     std::map<sf::Keyboard::Key, std::function<void()>> key_press;
     std::map<sf::Keyboard::Key, std::function<void()>> key_release;
